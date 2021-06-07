dab64bea98567d55eda81b4c83fe49514a98d34b
blas Ccarvajal <bcarvajal84@gmail.com>
2021-06-07 04:22:08 -0400

    10 - jQuery
    Se agrega jQuery Validation plugin

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
-function fechaActual(){
+function fechaActual() {
   var hoy = new Date();
   console.log(hoy);
-  
+
   var dia = hoy.getDate();
   var mes = hoy.getMonth();
-  var anno =  hoy.getFullYear();
+  var anno = hoy.getFullYear();
 
   var hora = hoy.getHours();
   var min = hoy.getMinutes();
   var seg = hoy.getSeconds();
 
   //var fecha =  hora+":"+min+":"+seg+" "+dia +"-"+mes+"-"+anno;
-  var fecha =  dia +"-"+mes+"-"+anno;
+  var fecha = dia + "-" + mes + "-" + anno;
 
   console.log(fecha);
   console.log("hola que tal");
 
   //document.getElementById("fecha").innerHTML = fecha;
-
 }
-  
 
-  function mensajeBienbenida(){
-      alert("bienvenido a la web del reciclaje");
-  }
+function fechaActual2() {
+  const tiempoTranscurrido = Date.now();
+  const hoy = new Date(tiempoTranscurrido);
+  console.log(hoy);
+  document.getElementById("fecha").innerHTML = hoy;
+}
 
-  function colorRandom(){
-    console.log("Ingreso a funcion colorRandom");
-    var numeroRamdon;
-    var sRandon = '#';
-    s = "0123456789ABCDEF";
+function mensajeBienbenida() {
+  alert("bienvenido a la web del reciclaje");
+}
 
-    for(var i=0; i<6;i++){
+function colorRandom() {
+  console.log("Ingreso a funcion colorRandom");
+  var numeroRamdon;
+  var sRandon = "#";
+  s = "0123456789ABCDEF";
+
+  for (var i = 0; i < 6; i++) {
     //numeroRamdon = Math.floor(Math.random()*16);
-     sRandon += s[Math.floor(Math.random()*16)]; 
+    sRandon += s[Math.floor(Math.random() * 16)];
     //console.log(numeroRamdon);
     console.log(sRandon);
   }
-    //document.getElementById("h1JumbotronIndex").innerHTML = sRandon;
-    var h1Index = document.getElementById("h1JumbotronIndex");
-    h1Index.style.color = sRandon;
-  }
-
-
-
+  //document.getElementById("h1JumbotronIndex").innerHTML = sRandon;
+  var h1Index = document.getElementById("h1JumbotronIndex");
+  h1Index.style.color = sRandon;
+}
 
